d23134d35da2a392ebf1fa0a8e37c203b50a6e51
traviswhittington88 <traviswhittington@ymail.com>
2019-12-23 15:18:06 -0600

    added comparePasswords function to AuthService

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,7 +1,14 @@
+const bcrypt = require('bcryptjs')
+
 const AuthService = {
   getUserWithUserName(db, user_name) {
-    
-  }
+    return db('blogful_users')
+      .where({ user_name })
+      .first()
+  },
+  comparePasswords(password, hash) {
+    return bcrypt.compare(password, hash)
+  },
 }
 
 module.exports = AuthService
